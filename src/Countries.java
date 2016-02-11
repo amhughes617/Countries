7dbd9ed96ea6c3b532704fe17ef93164fa21540c
Alex <amhughes@g.cofc.edu>
2016-02-11 13:14:42 -0500

    added read file method

--- src/Countries.java
+++ src/Countries.java
@@ -1,10 +1,33 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Countries {
-    public static void main(String[] args) {
+    public static HashMap<Character, ArrayList<Country>> countries = new HashMap<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        readFile("countries.txt");
 
+
+    }
+
+
+    static void readFile(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner fileScanner = new Scanner(f);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] lines = line.split("\\|");       //takes to backslashes to escape a functionality of | character in a String
+            Country country = new Country(lines[1], lines[0]);
+            if (!countries.containsKey(country.countryName.charAt(0))) {
+                countries.put(country.countryName.charAt(0), new ArrayList<>());
+            }
+            countries.get(country.countryName.charAt(0)).add(country);
+        }
+        System.out.println(countries);
     }
 
     static void writeFile(String fileName, String fileContent) throws IOException {
---
+++ src/Country.java
@@ -0,0 +1,28 @@
+/**
+ * Created by alexanderhughes on 2/11/16.
+ */
+public class Country {
+    String countryName;
+    String abbreviation;
+
+    public Country(String countryName, String abbreviation) {
+        this.countryName = countryName;
+        this.abbreviation = abbreviation;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+}
